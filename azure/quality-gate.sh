7a51218072affb4160a525d1b4a6d7428441a8bb
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2020-05-25 14:03:59 +0200

    add comments

--- azure/quality-gate.sh
+++ azure/quality-gate.sh
@@ -7,16 +7,18 @@ echo 'end' $(endtime)
 
 echo 'Start Keptn quality gate'
 
+### START THE EVALUTION
 json=$(curl -X POST "$KEPTN_ENDPOINT/v1/event" -H "accept: application/json" -H "x-token: $KEPTN_API_TOKEN" -H "Content-Type: application/json" -d "{ \"data\": { \"end\": \"$(endtime)\", \"project\": \"$KEPTN_PROJECT\", \"service\": \"$KEPTN_SERVICE\", \"stage\": \"$KEPTN_STAGE\", \"start\": \"$(starttime)\", \"teststrategy\": \"manual\" }, \"type\": \"sh.keptn.event.start-evaluation\", \"source\": \"devops-integration\"}" --insecure)
 context=$(echo $json | jq -r '.keptnContext')
 
 
 echo $context
 
-# evaluate quality gate
+# EVALUATE THE QUALITY GATE
 result=$(curl -X GET "$KEPTN_ENDPOINT/v1/event?keptnContext=$context&type=sh.keptn.events.evaluation-done" -H "accept: application/json" -H "x-token: $KEPTN_API_TOKEN" --insecure)
 httpcode=$(echo $result | jq -r '.code')
 
+### WAIT FOR THE QUALITY GATE TO RETURN WITH A RESULT
 while [ $httpcode -eq 500 ]
 do
   echo "return code is 500, sleeping for 30 seconds"
@@ -25,9 +27,9 @@ do
   httpcode=$(echo $result | jq -r '.code')
 done
 
-
 echo $result
 
+### ECHO THE RESULT
 decision=$(echo $result | jq -r '.data.evaluationdetails.result')
 echo $decision
 
