618eaa36a87546d34e78ae2003cd9e7487288a8b
jetzlstorfer <juergen.etzlstorfer@dynatrace.com>
2020-05-26 14:20:10 +0200

    fix typo

--- azure/quality-gate.sh
+++ azure/quality-gate.sh
@@ -5,7 +5,7 @@ echo 'Evaluate tests'
 echo 'start ' $starttime  
 echo 'start ' $STARTTIME
 echo 'start' $(starttime)
-echo 'start' $(testrun.startime)
+echo 'start' $(testrun.starttime)
 echo 'end' $(endtime)
 
 echo 'Start Keptn quality gate'
